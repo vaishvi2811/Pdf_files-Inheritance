4b1d17ee512d90c0a8e6cfa9785f012ae47f8a70
vaishvi2811 <vaishvi.khandelwal@gmail.com>
2025-01-28 12:30:44 +0530

    Login Signup UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Dashboard from './Components/Dashboard';
 import SectorsPage from './Components/SectorsPage';
 import StockWatchlist from './Components/StockWatchlist';
-import UserProfile from './Components/UserProfile';
+// import UserProfile from './Components/UserProfile';
 import Portfolio from './Components/Portfolio';
 import StockCard from './Components/StockCards';
 import StockDetail from './Components/StockPage';
